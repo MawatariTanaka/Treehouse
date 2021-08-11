16b075e7dc1b7b42abadcd8e255355bacfb167d6
MawatariTanaka <77230280+MawatariTanaka@users.noreply.github.com>
2021-08-11 22:15:44 +0700

    Javascipt Basics Complete

---
+++ Front End Web Development/07. JavaScript Basics/Workspace 6/js/else-if.js
@@ -0,0 +1,11 @@
+const weather = "sun"
+
+if (weather === 'sun') {
+    console.log("It's sunny, so I'm going swimming.");
+} else if (weather === 'rain') {
+    console.log("It's raining, so I'll read a book.");
+} else if (weather === 'snow') {
+    console.log("It's snowing, so I'm going sliding.");
+} else {
+    console.log("I don't know what I'm going to do today.")
+}
