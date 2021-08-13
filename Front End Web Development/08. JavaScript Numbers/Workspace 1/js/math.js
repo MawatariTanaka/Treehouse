4c72a8a791e2782ce5abcaeae5a8411e8fecc425
MawatariTanaka <77230280+MawatariTanaka@users.noreply.github.com>
2021-08-13 22:53:35 +0700

    Finish part 1 course 8

---
+++ Front End Web Development/08. JavaScript Numbers/Workspace 1/js/math.js
@@ -0,0 +1,10 @@
+const secsPerMin = 60;
+const minsPerHour = 60;
+const hoursPerDay = 24;
+const daysPerWeek = 7;
+const weeksPerYear = 52;
+
+const secondsPerDay = secsPerMin * minsPerHour * hoursPerDay;
+console.log(`There are ${secondsPerDay} seconds in a day.`);
+
+const yearsAlive = 15;
---
+++ Front End Web Development/08. JavaScript Numbers/Workspace 1/js/string-to-number.js
@@ -0,0 +1,8 @@
+const HTMLBadges = prompt('How many HTML badges do you have?');
+const CSSBadges = prompt('How many CSS badges do you have?');
+
+const totalBadges = parseInt(HTMLBadges) + +CSSBadges;
+console.log(totalBadges);
+
+const pi = prompt("What is pi?");
+console.log(+pi === 3.14);
